c1736657440be85421f7f45d0e446527f231eee9
tobitobita <tobitobita.jp@gmail.com>
2015-06-03 00:33:01 +0900

    #1 gulpでの基本タスク追加。

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,67 @@
 'use strict';
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var browser = require('browser-sync');
+var plumber = require('gulp-plumber');
 var del = require('del');
-var gulp = require("gulp");
-var concat = require("gulp-concat");
-var uglify = require("gulp-uglify");
-var browser = require("browser-sync");
-var plumber = require("gulp-plumber");
+var runSequence = require('run-sequence');
 
+// 変数定義
+var SRC_DIR = 'src/';
+var DEST_DIR = 'build/';
+var TEMP_DIR = 'tmp/';
 // フォルダ削除。
-gulp.task('clean', del.bind(null, ['tmp', 'build']));
+//gulp.task('clean', function () {
+//    return del.bind(null, [TEMP_DIR, DEST_DIR]);
+//});
+gulp.task('clean', del.bind(null, [TEMP_DIR, DEST_DIR]));
+
+// ファイル結合。
+gulp.task('concat', ['clean'], function () {
+    return gulp.src([SRC_DIR + 'js/*.js'])
+        .pipe(plumber())
+        .pipe(concat('sample-diagram.js'))
+        .pipe(gulp.dest(TEMP_DIR + 'js'))
+        ;
+});
+// jsを最小化。
+gulp.task('js', ['concat'], function () {
+    return gulp.src([TEMP_DIR + '/js/sample-diagram.js'])
+        .pipe(plumber())
+        .pipe(uglify())
+        .pipe(rename('sample-diagram.min.js'))
+        .pipe(gulp.dest(TEMP_DIR + 'js'))
+        ;
+});
+// ファイルコピー
+gulp.task('copy', ['js'], function () {
+    return gulp.src([SRC_DIR + '**/*.html', SRC_DIR + 'css/**'], {base: SRC_DIR})
+        .pipe(plumber())
+        .pipe(gulp.dest(DEST_DIR))
+        ;
+});
+// jsのコピー
+gulp.task('copy-js', ['copy'], function () {
+    return gulp.src([TEMP_DIR + 'js/**/*.min.js'], {base: TEMP_DIR})
+        .pipe(plumber())
+        .pipe(gulp.dest(DEST_DIR))
+        .pipe(browser.reload({stream: true}))
+        ;
+});
 
 // 簡易サーバーの起動。
-gulp.task("server", function () {
-    browser({
+gulp.task('server', ['copy-js'], function () {
+    return browser({
         server: {
-            baseDir: "./build"
+            baseDir: DEST_DIR
         }
     });
 });
 
-// ファイル結合。
-gulp.task("concat", function () {
-    var files = ['src/js/*.js'];
-    gulp.src(files)
-        .pipe(plumber())
-        .pipe(concat('sample-diagram.js'))
-        .pipe(gulp.dest('./temp/js'))
-        .pipe(browser.reload({stream: true}));
-});
-
-// jsを最小化。
-gulp.task("js", function () {
-    gulp.src(["js/**/*.js", "!js/min/**/*.js"])
-        .pipe(plumber())
-        .pipe(uglify())
-        .pipe(gulp.dest("./js/min"))
-        .pipe(browser.reload({stream: true}));
-});
-
-// ファイルコピー
-gulp.task('copy', function () {
-    return gulp.src(
-        ['src/*.html', 'src/css/**', 'src/js/*.js'],
-        {base: 'src'}
-    )
-        .pipe(gulp.dest('dest'));
-});
-
 // defaultのタスク。
-gulp.task("default", ['server'], function () {
-//    gulp.watch(["js/**/*.js", "!js/min/**/*.js"], ["js"]);
+gulp.task('default', ['server'], function () {
+    // ファイル監視
+    gulp.watch([SRC_DIR + 'js/**/*.js', SRC_DIR + '**/*.html'], ['copy']);
 });
